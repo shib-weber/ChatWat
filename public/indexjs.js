4af038a17551e8cc829e633778b6271574796b25
shibsrc <shibjyoti25@gmail.com>
2024-07-02 16:04:25 +0530

    app icon added

--- public/indexjs.js
+++ public/indexjs.js
@@ -2,3 +2,34 @@ const bt=document.querySelector("#bt")
 bt.addEventListener('click',()=>{
     location.href='signup.html'
 })
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, function(err) {
+        console.log('Service Worker registration failed:', err);
+      });
+    });
+  }
+
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById('installButton').style.display = 'block';
+
+  document.getElementById('installButton').addEventListener('click', (e) => {
+    document.getElementById('installButton').style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
